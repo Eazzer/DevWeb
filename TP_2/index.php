cfe631cbf050e85c136ea986f6014abc15a2b93f
Eazzer <43782559+Eazzer@users.noreply.github.com>
2019-05-13 16:03:08 +0200

    Add TP_2

---
+++ TP_2/index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html>
+<head>
+<title>Ma première page</title>
+<link rel="stylesheet" href="style.css">
+<meta charset="utf-8">
+</head>
+<body>
+
+
+<?php echo "bonjour"; toto ?>
+
+<ol>
+<li> <a href="#t1">titre1 </a></li>
+<li> <a href="#t2">titre2 </a></li>
+
+</ol>
+<h1 id="t1"> titre 1 </h1>
+<p>
+bla bla bla <br>bla bla bla <br>bla bla bla <br>bla bla bla <br>bla bla bla <br>
+bla bla bla <br>bla bla bla <br>bla bla bla <br>bla bla bla <br>bla bla bla <br>
+bla bla bla <br>bla bla bla <br>bla bla bla <br>bla bla bla <br>bla bla bla <br>
+</p>
+<h5 id="t2"> titre 2 </h5>
+<p>
+bla bla bla <br>bla bla bla <br>bla bla bla <br>bla bla bla <br>bla bla bla <br>
+bla bla bla <br>bla bla bla <br>bla bla bla <br>bla bla bla <br>bla bla bla <br>
+bla bla bla <br>bla bla bla <br>bla bla bla <br>bla bla bla <br>bla bla bla <br>
+</p>
+
+</body>
+</html>
